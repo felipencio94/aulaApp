5e7285a80f20ba297bb0776b8cf8a80b90a523b5
felipencio94 <feli.fuentealba@duocuc.cl>
2022-05-23 19:55:22 -0400

    db module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { HttpClientModule } from '@angular/common/http'; 
+import { SQLite } from '@ionic-native/sqlite/ngx';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }/*, SQLite*/],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
